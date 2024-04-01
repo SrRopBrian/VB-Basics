8a6400cdfd49551a00fd1b8ce899dc3bdc655b71
SrRopBrian <kipngenobrian070@gmail.com>
2024-04-01 12:05:24 +0300

    Grading System

---
+++ GradingSystem/Form1.Designer.vb
@@ -0,0 +1,76 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class GradingSystem
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        CheckGrade = New Button()
+        MarksLabel = New Label()
+        Marksdata = New TextBox()
+        SuspendLayout()
+        ' 
+        ' CheckGrade
+        ' 
+        CheckGrade.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        CheckGrade.Location = New Point(257, 225)
+        CheckGrade.Name = "CheckGrade"
+        CheckGrade.Size = New Size(148, 37)
+        CheckGrade.TabIndex = 0
+        CheckGrade.Text = "CHECK GRADE"
+        CheckGrade.UseVisualStyleBackColor = True
+        ' 
+        ' MarksLabel
+        ' 
+        MarksLabel.AutoSize = True
+        MarksLabel.Font = New Font("Segoe UI", 10F, FontStyle.Bold)
+        MarksLabel.Location = New Point(149, 180)
+        MarksLabel.Name = "MarksLabel"
+        MarksLabel.Size = New Size(82, 28)
+        MarksLabel.TabIndex = 1
+        MarksLabel.Text = "MARKS"
+        ' 
+        ' Marksdata
+        ' 
+        Marksdata.Font = New Font("Segoe UI", 10F)
+        Marksdata.Location = New Point(248, 174)
+        Marksdata.Name = "Marksdata"
+        Marksdata.Size = New Size(168, 34)
+        Marksdata.TabIndex = 2
+        ' 
+        ' GradingSystem
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(825, 474)
+        Controls.Add(Marksdata)
+        Controls.Add(MarksLabel)
+        Controls.Add(CheckGrade)
+        Name = "GradingSystem"
+        Text = "Grading System"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents CheckGrade As Button
+    Friend WithEvents MarksLabel As Label
+    Friend WithEvents Marksdata As TextBox
+
+End Class
---
+++ GradingSystem/Form1.vb
@@ -0,0 +1,33 @@
+﻿Public Class GradingSystem
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles CheckGrade.Click
+        Try
+            Dim marks As Decimal
+            If Decimal.TryParse(Marksdata.Text, marks) Then
+                Console.WriteLine("Conversion Successful.")
+            Else
+                MessageBox.Show("Error! Please enter valid marks!")
+            End If
+
+            If marks > 100 Then
+                MessageBox.Show("Error! Please enter valid marks (out of 100)!")
+            ElseIf marks >= 75 Then
+                MessageBox.Show("Way to go! You scored an A!")
+            ElseIf marks >= 65 Then
+                MessageBox.Show("Great! You scored a B!")
+            ElseIf marks >= 50 Then
+                MessageBox.Show("Keep on at it. You scored a C.")
+            Else
+                MessageBox.Show("You had a Fail. Try harder next time.")
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show("An error occurred." & ex.Message)
+        Finally
+            Marksdata.Clear()
+        End Try
+    End Sub
+
+    Private Sub GradingSystem_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
